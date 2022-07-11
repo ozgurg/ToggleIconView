438185aac3571c1e928f7b4e5948b32843e5dcea
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2022-07-11 06:26:32 +0300

    Init sample app

---
+++ app/src/main/java/og/android/sample/toggleiconview/MainActivity.kt
@@ -0,0 +1,11 @@
+package og.android.sample.toggleiconview
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
