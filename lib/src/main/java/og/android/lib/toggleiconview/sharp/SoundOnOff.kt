bdf60fb51f2677bbfe1546c344323dec58ad27d1
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2022-07-15 06:14:00 +0300

    Add sharp variant of SoundOnOff icon

---
+++ lib/src/main/java/og/android/lib/toggleiconview/sharp/SoundOnOff.kt
@@ -0,0 +1,17 @@
+package og.android.lib.toggleiconview.sharp
+
+import android.content.Context
+import android.util.AttributeSet
+import og.android.lib.toggleiconview.R
+import og.android.lib.toggleiconview.ToggleIconView
+
+class SoundOnOff @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) :
+    ToggleIconView(
+        context, attrs, defStyleAttr,
+        R.drawable.sharp_sound_on_to_off,
+        R.drawable.sharp_sound_off_to_on
+    )
